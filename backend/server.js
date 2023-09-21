aaee6d2b17c33618108c8ca08e388c9a9a725b84
Matthew Theseira <matthew.theseira@fdmgroup.com>
2023-09-21 14:28:29 +1000

    add stuff

--- backend/server.js
+++ backend/server.js
@@ -7,26 +7,39 @@ const cors = require("cors");
 const userRouter = require("./controllers/user");
 const authRouter = require("./controllers/auth");
 
-mongoose.connect(mongoString);
-const database = mongoose.connection;
-
-database.on("error", (error) => {
-  console.log(error);
-});
-
-database.once("connected", () => {
-  console.log("Database Connected");
-});
 const app = express();
 
+// Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
-app.use(cors());
 
+// Routes
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
-const PORT = process.env.PORT || 8080;
+// Database connection
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(mongoString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database Connected");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+connectToDatabase();
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: "Internal Server Error" });
+});
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server Started at ${PORT}`);
