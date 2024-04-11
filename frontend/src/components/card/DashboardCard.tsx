af5539ecf9d01d8818a5afd9bee96520ada1b64f
matthew theseira <matt_.t993@hotmail.com>
2024-04-11 18:28:17 +1000

    complete meal cards

--- frontend/src/components/card/DashboardCard.tsx
+++ frontend/src/components/card/DashboardCard.tsx
@@ -1,110 +1,86 @@
-import React from 'react'
+import React from "react";
 
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import BreakfastImage from "../../assets/images/breakfast.png";
+import { Box, Divider } from "@mui/material";
+import { FiPlusCircle } from "react-icons/fi";
+import { GiWheat } from "react-icons/gi";
+import { CgDropOpacity } from "react-icons/cg";
+import { FaFish } from "react-icons/fa";
 
 const DashboardCard = () => {
   return (
     <>
-    <div className='card__info'>
-    <Card className='card' sx={{height: 400 , backgroundColor: '#B2DFDB'   } } >
-    <CardContent>
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        Word of the Day
-      </Typography>
-      <Typography variant="h5" component="div">
-        context
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        adjective
-      </Typography>
-      <Typography variant="body2">
-        well meaning and kindly.
-        <br />
-        {'"a benevolent smile"'}
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small">Learn More</Button>
-    </CardActions>
-  </Card >
-  </div>
-  <div className='card__info'>
-  <Card className='card' sx={{height: 400 , backgroundColor: '#B2DFDB'   } }>
-    <CardContent>
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        Word of the Day
-      </Typography>
-      <Typography variant="h5" component="div">
-        context
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        adjective
-      </Typography>
-      <Typography variant="body2">
-        well meaning and kindly.
-        <br />
-        {'"a benevolent smile"'}
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small">Learn More</Button>
-    </CardActions>
-  </Card>
-  </div>
-  <div className='card__info'>
-  <Card className='card' sx={{height: 400 , backgroundColor: '#B2DFDB'   } }>
-    <CardContent>
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        Word of the Day
-      </Typography>
-      <Typography variant="h5" component="div">
-        context
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        adjective
-      </Typography>
-      <Typography variant="body2">
-        well meaning and kindly.
-        <br />
-        {'"a benevolent smile"'}
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small">Learn More</Button>
-    </CardActions>
-  </Card>
-  </div>
-  <div className='card__info'>
-  <Card className='card' sx={{height: 400 , backgroundColor: '#B2DFDB'   } }>
-    <CardContent>
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        Word of the Day
-      </Typography>
-      <Typography variant="h5" component="div">
-        context
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        adjective
-      </Typography>
-      <Typography variant="body2">
-        well meaning and kindly.
-        <br />
-        {'"a benevolent smile"'}
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small">Learn More</Button>
-    </CardActions>
-  </Card>
-  </div>
-  </>
-  
-  )
-}
+      <div className="card__info">
+        <Card className="card" sx={{ height: 400, backgroundColor: "#B2DFDB" }}>
+          <CardContent className="card__content">
+            <Box component="div" className="card__content--topSection">
+              <CardMedia
+                component="img"
+                sx={{ width: 130 }}
+                image={BreakfastImage}
+                alt="breakfast"
+              />
+              <Box component="div" className="card__content--info">
+                <Typography variant="h5" color="text.secondary" gutterBottom>
+                  Breakfast
+                </Typography>
+                <Typography variant="body1" >
+                  0 cals
+                </Typography>
+                <Typography variant="body1" >
+                  6 items
+                </Typography>
+              </Box>
+            </Box>
+            <Divider>
+              <FiPlusCircle className="card__content--icon" />
+            </Divider>
+            <Box component="div" className="card__content--bottomSection">
+              <Box component="div" className="bottomSection__stats">
+                <FaFish className="bottomSection__icon"  />
+                <Box component="div" className="bottomSection__info">
+                  <Typography variant="body1" >
+                    Carbs
+                  </Typography>
+                  <Typography variant="body1" >
+                    0g
+                  </Typography>
+                </Box>
+              </Box>
+              <Box component="div" className="bottomSection__stats">
+                <GiWheat className="bottomSection__icon"/>
+                <Box component="div" className="bottomSection__info">
+                  <Typography variant="body1" >
+                    Protein
+                  </Typography>
+                  <Typography variant="body1" >
+                    0g
+                  </Typography>
+                </Box>
+              </Box>
+              <Box component="div" className="bottomSection__stats">
+                <CgDropOpacity className="bottomSection__icon" />
+                <Box component="div" className="bottomSection__info">
+                  <Typography variant="body1" >
+                    Fat
+                  </Typography>
+                  <Typography variant="body1" >
+                    0g
+                  </Typography>
+                </Box>
+              </Box>
+            </Box>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
+};
 
-export default DashboardCard
+export default DashboardCard;
--- frontend/src/views/dashboard/DashBoard.tsx
+++ frontend/src/views/dashboard/DashBoard.tsx
@@ -1,81 +1,76 @@
-import React from 'react'
+import React from "react";
 
 import { GiWheat } from "react-icons/gi";
 import { CgDropOpacity } from "react-icons/cg";
 import { FaFish } from "react-icons/fa";
 
 import { FaBowlFood } from "react-icons/fa6";
-import  "./dashboard.css";
-import ProgressBar  from "../../components/progressBar/ProgressBar";
-import Datepicker from '../../components/datepicker/Datepicker';
-import DashboardCard from '../../components/card/DashboardCard';
-import MealCard from '../../components/card/MealCard';
-
+import "./dashboard.css";
+import ProgressBar from "../../components/progressBar/ProgressBar";
+import Datepicker from "../../components/datepicker/Datepicker";
+import DashboardCard from "../../components/card/DashboardCard";
+import MealCard from "../../components/card/MealCard";
 
 const Dashboard = () => {
-    const percentage: number = 66;
+  const percentage: number = 66;
   return (
     <>
-    <div className='header__container'>
-        <h1 className='title'>My Food Tracker</h1>
-        <div className='wrapper'>
-        <div className='stats__content'>
-        <div className='stats'>
-        <div className='stat'>
-        <FaBowlFood className='stat__icon' />
-        <div className='stat__info'>
-        <h3 className='stat__info--title'>Total</h3>
-        <p className='stat__info-number'>0 cals</p>
-        </div>
-        </div>
-        <div className='stat'>
-        <GiWheat className='stat__icon' />
-        <div className='stat__info'>
-        <h3 className='stat__info--title'>Carbs</h3>
-        <p className='stat__info-number'>0 cals</p>
-        </div>
-        </div>
-        <div className='stat'>
-        <FaFish className='stat__icon' />
-        <div className='stat__info'>
-        <h3 className='stat__info--title'>Protein</h3>
-        <p className='stat__info-number'>0g</p>
-        </div>
-        </div>
-        <div className='stat'>
-        <CgDropOpacity className='stat__icon' />
-        <div className='stat__info'>
-        <h3 className='stat__info--title'>Fat</h3>
-        <p className='stat__info-number'>0g</p>
-        </div>
-        </div>
-     
-        </div>
-        <p className='stats__text'>Aim to stay within 1550 calorie sweet spot.</p>
-        </div>
-        <div className='stat'>
-        <ProgressBar  percentage={percentage} />
-            </div>  
+      <div className="header__container">
+        <h1 className="title">My Food Tracker</h1>
+        <div className="wrapper">
+          <div className="stats__content">
+            <div className="stats">
+              <div className="stat">
+                <FaBowlFood className="stat__icon" />
+                <div className="stat__info">
+                  <h3 className="stat__info--title">Total</h3>
+                  <p className="stat__info-number">0 cals</p>
+                </div>
+              </div>
+              <div className="stat">
+                <GiWheat className="stat__icon" />
+                <div className="stat__info">
+                  <h3 className="stat__info--title">Carbs</h3>
+                  <p className="stat__info-number">0 cals</p>
+                </div>
+              </div>
+              <div className="stat">
+                <FaFish className="stat__icon" />
+                <div className="stat__info">
+                  <h3 className="stat__info--title">Protein</h3>
+                  <p className="stat__info-number">0g</p>
+                </div>
+              </div>
+              <div className="stat">
+                <CgDropOpacity className="stat__icon" />
+                <div className="stat__info">
+                  <h3 className="stat__info--title">Fat</h3>
+                  <p className="stat__info-number">0g</p>
+                </div>
+              </div>
             </div>
-    </div>
-    <div className='datepicker'>
-    <Datepicker/>
-    </div> 
-    <div className='cards'>
-    <DashboardCard/>
-    </div>
-    <div className='cards'>
-    <MealCard/>
-    </div>
-    
-
-
-   
-    <div className='overlay'>
-        
+            <p className="stats__text">
+              Aim to stay within 1550 calorie sweet spot.
+            </p>
+          </div>
+          <div className="stat">
+            <ProgressBar percentage={percentage} />
+          </div>
         </div>
-    </>
-  )
-}
+      </div>
+      <div className="datepicker">
+        <Datepicker />
+      </div>
+      <div className="cards">
+        <DashboardCard />
+      </div>
+      <div className="cards">
+        <MealCard />
+      </div>
 
-export default Dashboard
+      <div className="overlay"></div>
+    </>
+  );
+};
+
+export default Dashboard;
